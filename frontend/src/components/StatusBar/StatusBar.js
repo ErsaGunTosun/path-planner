69551fe43d34ab165b0d4b921f019eca56f82985
Ersagun Tosun <ersagunfatihdev@gmail.com>
2025-07-18 10:06:46 +0300

    UPDATE status bar status bar added bookmark save button

--- frontend/src/components/StatusBar/StatusBar.js
+++ frontend/src/components/StatusBar/StatusBar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function StatusBar({ isMarkeMode, isObstacleMode, pathCreateFunction, clearMapFunction }) {
 
-  
+
 
     return (
         <div className="status-bar fixed z-50 left-12 top-5 flex gap-2">
@@ -26,13 +26,19 @@ function StatusBar({ isMarkeMode, isObstacleMode, pathCreateFunction, clearMapFu
                 className="flex items-center space-x-2 px-3 py-2 rounded-full bg-white">
                 <span className="text-gray-800 text-sm font-medium whitespace-nowrap">Status
                     <span className={`ml-2 text-xs font-semibold ${isMarkeMode ? 'text-green-500' : isObstacleMode ? 'text-red-500' : 'text-gray-500'}`}>
-                        {   
+                        {
 
                             isMarkeMode ? "Marker Mode" : isObstacleMode ? "Obstacle Mode" : "Move Mode"
                         }
                     </span>
                 </span>
             </p>
+
+            <button
+                className="flex items-center space-x-2 px-3 py-2 rounded-full bg-white hover:bg-gray-100 ">
+                <span className="text-gray-800 text-sm font-medium whitespace-nowrap">Save Path
+                </span>
+            </button>
 
         </div>
     )
